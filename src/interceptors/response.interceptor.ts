bbfcb1636ae0b24ff27d6a76658f9640f3ec1970
shukhratovich-a <shukhratovich138@gmail.com>
2023-07-26 22:53:39 +0500

    Create response interceptor

--- src/interceptors/response.interceptor.ts
+++ src/interceptors/response.interceptor.ts
@@ -14,7 +14,8 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
   constructor(private reflector: Reflector) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
-    const message = this.reflector.get<string>(ResponseMessageKey, context.getHandler()) ?? "";
+    const message =
+      this.reflector.get<string>(ResponseMessageKey, context.getHandler()) ?? undefined;
 
     return next.handle().pipe(
       map((data) => ({
