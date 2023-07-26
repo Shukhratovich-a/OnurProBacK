ba7791b9edafc7c1f8d7865d1caf768199a57c0f
shukhratovich-a <shukhratovich138@gmail.com>
2023-07-26 22:54:29 +0500

    Connect modules

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,6 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+
+import { AdminModule } from "./admin/admin.module";
+import { AboutModule } from "./about/about.module";
+import { ServiceModule } from "./service/service.module";
+import { FilesModule } from "./files/files.module";
 
 @Module({
-  imports: [],
+  imports: [AdminModule, AboutModule, ServiceModule, FilesModule],
 })
 export class Modules {}
